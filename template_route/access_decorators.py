f99b18d895a6201fa9d741486db087cc6f329fcd
Nilesh Gawli <nbgawli@gmail.com>
2023-03-11 07:36:51 +0530

    Added Education Profile Page

--- template_route/access_decorators.py
+++ template_route/access_decorators.py
@@ -1,12 +1,11 @@
 from django.http import HttpResponse
-import status
-
+from rest_framework import status
 def un_authenticated_user(Wrapped):
-    def wrapper(self,request,*args, **kwargs):
-    
+    def wrapper(request,*args, **kwargs):
+
         print(f'request:{request.user}')
         if (not request.user.is_authenticated) and request.user == "AnonymousUser":
-            return Wrapped(self,request,*args, **kwargs)
+            return Wrapped(request,*args, **kwargs)
         else:
             return HttpResponse('Your are not logged in user', status=status.HTTP_401_UNAUTHORIZED)
     return wrapper
